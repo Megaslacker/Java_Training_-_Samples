eda6458c8ac561fe5b4e8cf95736e7789da64e5d
Kotok_Artem <megaslacker@tut.by>
2018-09-28 10:29:51 +0300

    1 commit

---
+++ src/GitHubTestClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kotok_artem on 28.09.2018.
+ */
+public class GitHubTestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
